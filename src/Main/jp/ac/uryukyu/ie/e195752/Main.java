4dd302f06ebbbaea22d58b527fab14df4a1714bc
Soya Uehara <e195752@ie.u-ryukyu.ac.jp>
2019-12-10 17:07:41 +0900

    report4 3rd

--- src/Main/jp/ac/uryukyu/ie/e195752/Main.java
+++ src/Main/jp/ac/uryukyu/ie/e195752/Main.java
@@ -2,14 +2,7 @@ package jp.ac.uryukyu.ie.e195752;
 
 public class Main {
     public static void main(String[] args){
-        try {
-            String str = null;
-            str.length();
-        }
-        catch (Exception e){
-            System.out.println("Exceptionが発生したよ");
-            System.out.println(e.getMessage());
-        }
-
+        String str = "百二十三";
+        Integer.parseInt(str);
     }
 }
